277e96aac5a6a4ed0769e0c4b1c0139c6d45e683
Bryan García <bryan1997st@gmail.com>
2021-05-02 16:59:43 -0500

    wrap component to add title and cards inside a row

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -8,15 +8,17 @@ export const Cards = ({ group }) => {
 
 	return (
 		/* Map card */
-		<div className="card-deck">
-			<div className="">{store[group].title}</div>
-			{store[group].results.length >= 10 ? (
-				store[group].results.map((element, index) => {
-					return <Card key={element.uid} uid={element.uid} attribute={store[group].attribute} />;
-				})
-			) : (
-				<div>{"Loading"}</div>
-			)}
+		<div className="row my-5">
+			<div className="cards-title my-3">{store[group].title}</div>
+			<div className="card-deck card-riel d-flex justify-content-between flex-row flex-nowrap">
+				{store[group].results.length >= 10 ? (
+					store[group].results.map((element, index) => {
+						return <Card key={element.uid} uid={element.uid} attribute={store[group].attribute} />;
+					})
+				) : (
+					<div>{"Loading"}</div>
+				)}
+			</div>
 		</div>
 	);
 };
