7765edf97ab5268b86bbd8c78955fb9aab9c6889
Bryan García <bryan1997st@gmail.com>
2021-05-23 22:59:15 -0500

    Add params and store to the component

---
+++ src/js/views/LearnMore.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export function LearnMore() {
+	const { store, actions } = useContext(Context);
+	const { uid, attribute} = useParams();
+	const characterInfo = actions.getCharacter(uid, attribute);
+	
+    return {
+        
+    };
+}
