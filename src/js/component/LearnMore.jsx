f0faab0d7f75d9165922388192a1a45f94f9bb3d
Bryan García <bryan1997st@gmail.com>
2021-05-23 22:11:58 -0500

    Create the component and import packages

---
+++ src/js/component/LearnMore.jsx
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+
+export function LearnMore() {
+    const { store, actions} = useContext(Context);
+
+    
+    return {
+
+    }
+}
